e2a4f3334b5f0d6a78224c364f541f4199347a5a
coldturkeymeat <nathanchee95@gmail.com>
2023-02-06 16:11:40 +1100

    updates to all files

--- script.js
+++ script.js
@@ -1,32 +1,95 @@
-const boardSize = 9;
-
+const roundForm = document.getElementById("roundForm");
 
 const gameBoard = (() => {
-    let board = new Array (boardSize);
+    const boardSize = 9;
 
-    const clearBoard = () => board
-    const addPosition = (board, newPos) {
-        if (board[newPos])
+    const createBoard = () => {
+        const gridPositions = document.getElementsByClassName("grid-pos");
+        let board = new Array(boardSize).fill("");
+        
+        for (let i=0; i < gridPositions.length; i++) {
+            gridPositions[i].innerHTML = board[i];
+            gridPositions[i].addEventListener("click", addPosition);           
+        };
+        return board;
+    };
+
+    let newBoard = createBoard();
+    const getBoard = () => newBoard;
+    const clearBoard = (board) => board = new Array (boardSize).fill("");
+        
+    const addPosition = () => {
+        const positionId = e.originalTarget.id;
+        if (!newBoard[positionId]) {
+            newBoard[positionId] = displayController.getCurrPlayer.getSymbol;
+            displayController.getCurrPlayer.addPos(positionId);
+            displayController.getCurrPlayer.setCurrPlayer(false);   
+        }
+        // if (board[newPos])
     }
-})
+    return {getBoard, clearBoard, addPosition};
+})();
 
-const Player = (name) => {
+const Player = (name, symbol) => {
     const getName = () => name;
-    return {getName};
+    const getSymbol = () => symbol;
+    let currPlayer = false;
+    const setCurrPlayer = (bool) => currPlayer = bool;
+    const isCurrPlayer = () => currPlayer;
+    const positions = new Array();
+    const addPos = (num) => {
+        if (positions.length === 0 || num > positions[positions.length - 1]) {
+            positions.push(num);
+        } else if (num < positions[0]) {
+            positions.unshift(num);
+        } else {
+            for (let i = 0; i < positions.length - 1; i++) {
+            if (num > positions[i] && num < positions[i + 1]) {
+                positions.splice(i + 1, 0, num);
+                break;
+            }
+            }
+        }          
+    }
+    return {getName, getSymbol, addPos, isCurrPlayer, setCurrPlayer};
 }
 
 const displayController = (() => {
-    let round = 1;
-    let firstPlayer
+    let currentPlayer;
+    const setCurrPlayer = (player) => {
+        currentPlayer = player;
+        player.setCurrPlayer(true);
+    }
 
-    const newGame = (rounds) => {
-        round = 1;
-        gameBoard.clearBoard();
+    const newGame = (rounds, p1, p2) => {
+        let round = 1;
+        playGame(round, rounds, p1, p2);
     }
-    const incrementRound = () => {
-        (round < rounds) ? round++ : 0;
+
+    const displayBoard = () => {
+        const board = gameBoard.getBoard();
+
     }
-    const getRound = () => round;
-    const currentPlayer = ()
-    return {getRound, incrementRound};
-)}
+
+    const playGame = (currentRound, numRounds, playerOne, playerTwo) => {
+        setCurrPlayer(playerOne);
+        while(currentRound < numRounds) {
+            
+        }
+    }
+    return {getRound, incrementRound, newGame, getCurrPlayer};
+})();
+
+const play = (e) => {
+    e.preventDefault();
+    const numRounds = document.getElementById("numRounds").value;
+    const playerOne = document.getElementById("playerOne").value;
+    const playerTwo = document.getElementById("playerTwo").value;
+
+    const p1 = Player(playerOne,'x');
+    const p2 = Player(playerTwo,'o');
+    displayController.newGame(numRounds,p1, p2);
+    console.log(numRounds);
+}
+
+roundForm.onsubmit = play;
